ceee695e67751cd687620a73bc5eac73ed75ba2d
Jonathan <jmedel.7@gmail.com>
2020-03-04 16:24:06 -0300

    crud ajax demo alright

--- app/assets/javascripts/mushrooms.js
+++ app/assets/javascripts/mushrooms.js
@@ -50,6 +50,48 @@ $(document).ready(function(){
         });
       } 
     })
+    //Delete
+    $('ul').on('click', '.delete_comment',function(e){
+      e.preventDefault()
+      var id = $(this).closest('li')[0].id.split('_')[1]
+      $.ajax({
+        beforeSend: function (xhr) { 
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) 
+          },
+        type: "delete",
+        url: "/comments/" + id,
+        dataType: "script"
+      });
+      
+    })
+
+    // Edit a pinche commment
+    $('ul').on('click', '.update_comment',function(e){
+      e.preventDefault()
+      var id = $(this).closest('li')[0].id.split('_')[1]
+      $.ajax({
+        type: "get",
+        url: "/comments/" + id +" /edit",
+        dataType: "script"
+      });
+    })
+    // Update a pinche comment
+    $('#update').on('click', 'button', function(e){
+      e.preventDefault()
+      var content = $(this).closest('form')[0][0].value
+      var id = $(this).closest('form')[0][1].value
+      
+      $.ajax({
+        beforeSend: function (xhr) { 
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) 
+          },
+        type: "patch",
+        url: "comments/" + id,
+        data: { comment: content },
+        dataType: "script"
+        
+      });
+    })
 
   })
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,21 @@ class CommentsController < ApplicationController
     @comment = Comment.new(content: params[:comment])
     @comment.save
   end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy 
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+
+  end
+  
+  def update
+    @comment = Comment.find(params[:id])
+    # @comment.update(content: params[:content])
+    @comment.content = params[:comment]
+    @comment.save
+  end
 end
